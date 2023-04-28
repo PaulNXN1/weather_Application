2dcd362107025ee8df237a78ef50be865d129f0b
Paul Nixon <paulnxn1@gmail.com>
2023-04-27 21:46:15 -0400

    Added buttons for history

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 var newButton = document.getElementById('newButton');
 var inputField = document.getElementById('input');
 var correctTime = document.getElementById('timeDate');
+var historyList = [];
+var finalSearch = document.getElementById('pastSearch');
+
+function loadInput() {
+    getAPI(inputField.value);
+    historyList.push(inputField.value);
+    localStorage.setItem('addList', JSON.stringify(historyList));
+    renderCities();
+}
+
+function init(){
+    // gets data from localstorage if available
+    var listTemp = localStorage.getItem("addList");
+    if(listTemp){ // if exists
+      historyList = JSON.parse(listTemp);
+    }
+    renderCities();
+  }
+ 
+  init();
+
+// Code for dayJS
 
 let Day = dayjs();
 correctTime.textContent = Day.format('MMM DD, YYYY');
 
+// main function that pulls API 
 
-function getAPI() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + inputField.value + '&APPID=938fb3720289098948304613a4e3a426&units=imperial';
+function getAPI(city) {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=938fb3720289098948304613a4e3a426&units=imperial';
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
@@ -40,6 +63,8 @@ function getAPI() {
                 .then(function (data) {
                     console.log(data);
 
+                    document.getElementById('display').innerHTML = "";
+
                     for (var i = 0; i < data.list.length; i++) {
                         if (data.list[i].dt_txt.includes('12:00:00')) {
                             var forecastCard = document.createElement('div');
@@ -48,25 +73,25 @@ function getAPI() {
                             var innerHumidity = document.createElement('p');
                             var weatherIcon = document.createElement('p');
                             var newDay = document.createElement('p');
+                            var weatherImage = document.createElement('img');
+                           
 
                             // var formatDate = Date.parse(data.list[i].dt_txt);
 
                             var dayWeather = dayjs.unix(data.list[i].dt).format('MMM D, YYYY');
                           
-
+                                console.log(`https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`);
                     
-                    
+                           // `https://openweathermap.org/img/wn/10d@2x.png`   - the URL for the icon page. 
                     
                             newDay.textContent = dayWeather;
-
-
-                            innerTemp.textContent = "Temp " + data.list[i].main.temp;
-                            innerWind.textContent = "Wind " + data.list[i].wind.speed + " MPH";
-                            innerHumidity.textContent = "Humidity " + data.list[i].main.humidity + "%";
-                    
+                            innerTemp.textContent = "Temp: " + data.list[i].main.temp;
+                            innerWind.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
+                            innerHumidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+                            weatherImage.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`);
 
                         
-                            forecastCard.append(newDay, innerTemp, innerWind, innerHumidity, weatherIcon);
+                            forecastCard.append(newDay, innerTemp, innerWind, innerHumidity, weatherImage);
 
                             document.getElementById('display').append(forecastCard);
 
@@ -81,17 +106,41 @@ function getAPI() {
 }
 
 
+
 // Event listener - when user clicks on Search Button. 
 
 newButton.addEventListener('click', function (event) {
 
     event.preventDefault();
 
-    getAPI();
+    loadInput();
 
 });
 
 
+function renderCities() {
 
+    finalSearch.innerHTML = "";
+
+    for (var i = 0; i < historyList.length; i++) {
+        let city = historyList[i];
+        let cityButton = document.createElement('button');
+
+        cityButton.style.background = 'grey';
+        cityButton.style.borderRadius = '10px';
+
+        cityButton.dataset.city = city;
+        cityButton.value = city;
+        cityButton.textContent = city;
+        finalSearch.appendChild(cityButton);
+
+        cityButton.addEventListener('click', function(event){
+            
+            getAPI(event.target.dataset.city);
+
+        })
+    }
+
+}
 
 
